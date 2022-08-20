ffdcdb6aa2388ca172b1904d4ddc0aa8a779fae6
Yang Zou <yang0201@outlook.com>
2022-08-20 17:11:12 +0100

    Add simple tests

--- shopping_list/tests.py
+++ shopping_list/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 
-# Create your tests here.
+from shopping_list.models import ShoppingItem, ShoppingList
+
+
+@pytest.mark.django_db
+def test_valid_shopping_list_is_created():
+    url = reverse("all-shopping-lists")
+    data = {
+        "name": "Groceries",
+    }
+    client = APIClient()
+    response = client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_201_CREATED
+    assert ShoppingList.objects.get().name == "Groceries"
+
+
+def test_shopping_list_name_missing_returns_bad_request():
+    url = reverse("all-shopping-lists")
+    data = {"something_else": "blahblah"}
+    client = APIClient()
+    response = client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
