bd9c506ce39e367603a711281ca491edea5be97e
Yang Zou <yang0201@outlook.com>
2022-08-20 16:28:09 +0100

    Add nested serializer for shopping list

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from shopping_list.models import ShoppingItem
+from shopping_list.models import ShoppingItem, ShoppingList
 
 
 class ShoppingItemSerializer(serializers.ModelSerializer):
@@ -7,3 +7,11 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
         model = ShoppingItem
         fields = ["id", "name", "purchased"]
         read_only_fields = ("id",)
+
+
+class ShoppingListSerializer(serializers.ModelSerializer):
+    shopping_items = ShoppingItemSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = ShoppingList
+        fields = ["id", "name", "shopping_items"]
