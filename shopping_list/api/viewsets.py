c42ef84416ee9c18d0adae5a5ca2c573ce39775a
Yang Zou <yang0201@outlook.com>
2022-08-20 15:04:17 +0100

    Add delete-purchased custom action

--- shopping_list/api/viewsets.py
+++ shopping_list/api/viewsets.py
@@ -1,3 +1,6 @@
+from rest_framework import status
+from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from shopping_list.api.serializers import ShoppingItemSerializer
 from shopping_list.models import ShoppingItem
@@ -6,3 +9,11 @@ from shopping_list.models import ShoppingItem
 class ShoppingItemViewSet(ModelViewSet):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
+
+    @action(
+        detail=False,
+        methods=["DELETE"],
+    )
+    def delete_purchased(self, request):
+        ShoppingItem.objects.filter(purchased=True).delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
