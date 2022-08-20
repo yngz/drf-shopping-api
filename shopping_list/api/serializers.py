22f52aa1e6bf1d492a434a6869d8076c87e3a7cf
Yang Zou <yang0201@outlook.com>
2022-08-20 14:03:07 +0100

    Make id read-only field

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -6,3 +6,4 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ShoppingItem
         fields = ["id", "name", "purchased"]
+        read_only_fields = ("id",)
