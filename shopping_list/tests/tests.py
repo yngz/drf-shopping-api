3b63dc684297e3e16ff8aa8fbb194dfc4eb6b9be
Yang Zou <yang0201@outlook.com>
2022-08-23 15:27:19 +0100

    Test ShoppingItem CRUD actions

--- shopping_list/tests/tests.py
+++ shopping_list/tests/tests.py
@@ -150,3 +150,115 @@ def test_shopping_list_is_deleted():
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert len(ShoppingList.objects.all()) == 0
+
+
+@pytest.mark.django_db
+def test_valid_shopping_item_is_created():
+    shopping_list = ShoppingList.objects.create(name="Groceries")
+    url = reverse("add-shopping-item", args=[shopping_list.id])
+
+    data = {"name": "Milk", "purchased": False}
+    client = APIClient()
+    response = client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_create_shopping_item_missing_data_returns_bad_request():
+    shopping_list = ShoppingList.objects.create(name="Groceries")
+    url = reverse("add-shopping-item", args=[shopping_list.id])
+
+    data = {
+        "name": "Milk",
+    }
+    client = APIClient()
+    response = client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_shopping_item_is_retrieved_by_id(create_shopping_item):
+    shopping_item = create_shopping_item(name="Chocolate")
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+    client = APIClient()
+
+    response = client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+    assert response.data["name"] == "Chocolate"
+
+
+@pytest.mark.django_db
+def test_change_shopping_item_purchased_status(create_shopping_item):
+    shopping_item = create_shopping_item(name="Chocolate")
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+    client = APIClient()
+
+    data = {"name": "Chocolate", "purchased": True}
+    response = client.put(url, data, format="json")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert ShoppingItem.objects.get().purchased is True
+
+
+@pytest.mark.django_db
+def test_change_shopping_item_purchased_status_with_missing_data_returns_bad_request(
+    create_shopping_item,
+):
+    shopping_item = create_shopping_item(name="Chocolate")
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+    client = APIClient()
+
+    data = {"purchased": True}
+    response = client.put(url, data, format="json")
+
+    assert response.status_code == status.HTTP_400_BAD_REQUEST
+
+
+@pytest.mark.django_db
+def test_change_shopping_item_purchased_status_with_partial_update(
+    create_shopping_item,
+):
+    shopping_item = create_shopping_item(name="Chocolate")
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+    client = APIClient()
+
+    data = {"purchased": True}
+    response = client.patch(url, data, format="json")
+
+    assert response.status_code == status.HTTP_200_OK
+    assert ShoppingItem.objects.get().purchased is True
+
+
+@pytest.mark.django_db
+def test_shopping_item_is_deleted(create_shopping_item):
+    shopping_item = create_shopping_item(name="Chocolate")
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+
+    client = APIClient()
+    response = client.delete(url)
+
+    assert response.status_code == status.HTTP_204_NO_CONTENT
+    assert len(ShoppingItem.objects.all()) == 0
