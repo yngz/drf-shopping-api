1cb67abca9e4f6bf2cf00c7c0635eecad4a53b5b
Yang Zou <yang0201@outlook.com>
2022-08-24 12:27:56 +0100

    Add members to shopping list

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class ShoppingList(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
+    members = models.ManyToManyField("auth.User")
 
     def __str__(self):
         return self.name
