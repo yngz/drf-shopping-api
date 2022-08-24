58f698ce659068eadcaa58213aaff50d30de1fa2
Yang Zou <yang0201@outlook.com>
2022-08-24 12:41:37 +0100

    Include members in shopping list serializer

--- shopping_list/api/serializers.py
+++ shopping_list/api/serializers.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from shopping_list.models import ShoppingItem, ShoppingList
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username"]
 
 
 class ShoppingItemSerializer(serializers.ModelSerializer):
@@ -16,7 +23,8 @@ class ShoppingItemSerializer(serializers.ModelSerializer):
 
 class ShoppingListSerializer(serializers.ModelSerializer):
     shopping_items = ShoppingItemSerializer(many=True, read_only=True)
+    members = UserSerializer(many=True, read_only=True)
 
     class Meta:
         model = ShoppingList
-        fields = ["id", "name", "shopping_items"]
+        fields = ["id", "name", "shopping_items", "members"]
