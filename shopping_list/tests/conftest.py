e6d509cb192fceef8de7c83c4048bfd378ca2e6e
Yang Zou <yang0201@outlook.com>
2022-08-25 14:46:46 +0100

    Update tests with members and add more tests for permissions

--- shopping_list/tests/conftest.py
+++ shopping_list/tests/conftest.py
@@ -6,8 +6,9 @@ from shopping_list.models import ShoppingItem, ShoppingList
 
 @pytest.fixture(scope="session")
 def create_shopping_item():
-    def _create_shopping_item(name):
+    def _create_shopping_item(name, user):
         shopping_list = ShoppingList.objects.create(name="My shopping list")
+        shopping_list.members.add(user)
         shopping_item = ShoppingItem.objects.create(
             name=name, purchased=False, shopping_list=shopping_list
         )
@@ -32,3 +33,14 @@ def create_authenticated_client():
         return client
 
     return _create_authenticated_client
+
+
+@pytest.fixture(scope="session")
+def create_shopping_list():
+    def _create_shopping_list(user):
+        shopping_list = ShoppingList.objects.create(name="Groceries")
+        shopping_list.members.add(user)
+
+        return shopping_list
+
+    return _create_shopping_list
--- shopping_list/tests/tests.py
+++ shopping_list/tests/tests.py
@@ -1,7 +1,7 @@
 import pytest
+from django.contrib.auth.models import User
 from django.urls import reverse
 from rest_framework import status
-from rest_framework.test import APIClient
 from shopping_list.models import ShoppingItem, ShoppingList
 
 
@@ -18,22 +18,26 @@ def test_valid_shopping_list_is_created(create_user, create_authenticated_client
     assert ShoppingList.objects.get().name == "Groceries"
 
 
-def test_shopping_list_name_missing_returns_bad_request():
+@pytest.mark.django_db
+def test_shopping_list_name_missing_returns_bad_request(
+    create_user, create_authenticated_client
+):
     url = reverse("all-shopping-lists")
     data = {"something_else": "blahblah"}
-    client = APIClient()
+
+    client = create_authenticated_client(create_user())
     response = client.post(url, data, format="json")
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 @pytest.mark.django_db
-def test_all_shopping_lists_are_listed():
+def test_all_shopping_lists_are_listed(create_user, create_authenticated_client):
     url = reverse("all-shopping-lists")
     ShoppingList.objects.create(name="Groceries")
     ShoppingList.objects.create(name="Books")
 
-    client = APIClient()
+    client = create_authenticated_client(create_user())
     response = client.get(url)
 
     assert response.status_code == status.HTTP_200_OK
@@ -43,11 +47,14 @@ def test_all_shopping_lists_are_listed():
 
 
 @pytest.mark.django_db
-def test_shopping_list_is_retrieved_by_id():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_shopping_list_is_retrieved_by_id(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
-    client = APIClient()
 
     response = client.get(url, format="json")
 
@@ -56,10 +63,31 @@ def test_shopping_list_is_retrieved_by_id():
 
 
 @pytest.mark.django_db
-def test_shopping_list_includes_only_corresponding_items():
+def test_admin_can_retrieve_shopping_list(
+    create_user, create_shopping_list, admin_client
+):
+
+    user = create_user()
+    shopping_list = create_shopping_list(user)
+
+    url = reverse("shopping-list-detail", args=[shopping_list.id])
+
+    response = admin_client.get(url, format="json")
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_shopping_list_includes_only_corresponding_items(
+    create_user, create_authenticated_client
+):
+    user = create_user()
+    client = create_authenticated_client(user)
 
     shopping_list = ShoppingList.objects.create(name="Groceries")
+    shopping_list.members.add(user)
     another_shopping_list = ShoppingList.objects.create(name="Books")
+    another_shopping_list.members.add(user)
 
     ShoppingItem.objects.create(
         shopping_list=shopping_list, name="Eggs", purchased=False
@@ -69,7 +97,6 @@ def test_shopping_list_includes_only_corresponding_items():
     )
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
-    client = APIClient()
     response = client.get(url)
 
     assert len(response.data["shopping_items"]) == 1
@@ -77,8 +104,12 @@ def test_shopping_list_includes_only_corresponding_items():
 
 
 @pytest.mark.django_db
-def test_shopping_list_name_is_changed():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_shopping_list_name_is_changed(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
 
@@ -86,7 +117,6 @@ def test_shopping_list_name_is_changed():
         "name": "Food",
     }
 
-    client = APIClient()
     response = client.put(url, data=data, format="json")
 
     assert response.status_code == status.HTTP_200_OK
@@ -94,23 +124,32 @@ def test_shopping_list_name_is_changed():
 
 
 @pytest.mark.django_db
-def test_shopping_list_not_changed_because_name_missing():
-
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_shopping_list_not_changed_because_name_missing(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
 
     data = {"something_else": "blahblah"}
 
-    client = APIClient()
     response = client.put(url, data=data, format="json")
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 @pytest.mark.django_db
-def test_shopping_list_name_is_changed_with_partial_update():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_update_shopping_list_restricted_if_not_member(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(shopping_list_creator)
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
 
@@ -118,7 +157,24 @@ def test_shopping_list_name_is_changed_with_partial_update():
         "name": "Food",
     }
 
-    client = APIClient()
+    response = client.put(url, data=data, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_shopping_list_name_is_changed_with_partial_update(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
+    url = reverse("shopping-list-detail", args=[shopping_list.id])
+
+    data = {
+        "name": "Food",
+    }
+
     response = client.patch(url, data=data, format="json")
 
     assert response.status_code == status.HTTP_200_OK
@@ -126,26 +182,54 @@ def test_shopping_list_name_is_changed_with_partial_update():
 
 
 @pytest.mark.django_db
-def test_partial_update_with_missing_name_has_no_impact():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_partial_update_with_missing_name_has_no_impact(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
 
     data = {"something_else": "blahblah"}
 
-    client = APIClient()
     response = client.patch(url, data=data, format="json")
 
     assert response.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.django_db
-def test_shopping_list_is_deleted():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_partial_update_shopping_list_restricted_if_not_member(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(shopping_list_creator)
 
     url = reverse("shopping-list-detail", args=[shopping_list.id])
 
-    client = APIClient()
+    data = {
+        "name": "Food",
+    }
+
+    response = client.patch(url, data=data, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_shopping_list_is_deleted(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
+
+    url = reverse("shopping-list-detail", args=[shopping_list.id])
+
     response = client.delete(url)
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
@@ -153,40 +237,105 @@ def test_shopping_list_is_deleted():
 
 
 @pytest.mark.django_db
-def test_valid_shopping_item_is_created():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_delete_shopping_list_restricted_if_not_member(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(shopping_list_creator)
+
+    url = reverse("shopping-list-detail", args=[shopping_list.id])
+
+    response = client.delete(url, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_valid_shopping_item_is_created(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
+
     url = reverse("add-shopping-item", args=[shopping_list.id])
 
     data = {"name": "Milk", "purchased": False}
-    client = APIClient()
+
     response = client.post(url, data, format="json")
 
     assert response.status_code == status.HTTP_201_CREATED
 
 
 @pytest.mark.django_db
-def test_create_shopping_item_missing_data_returns_bad_request():
-    shopping_list = ShoppingList.objects.create(name="Groceries")
+def test_create_shopping_item_missing_data_returns_bad_request(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list = create_shopping_list(user)
+
     url = reverse("add-shopping-item", args=[shopping_list.id])
 
     data = {
         "name": "Milk",
     }
-    client = APIClient()
+
     response = client.post(url, data, format="json")
 
     assert response.status_code == status.HTTP_400_BAD_REQUEST
 
 
 @pytest.mark.django_db
-def test_shopping_item_is_retrieved_by_id(create_shopping_item):
-    shopping_item = create_shopping_item(name="Chocolate")
+def test_not_member_of_list_can_not_add_shopping_item(
+    create_user, create_authenticated_client, create_shopping_list
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    shopping_list = create_shopping_list(shopping_list_creator)
+
+    url = reverse("add-shopping-item", args=[shopping_list.id])
+
+    data = {"name": "Milk", "purchased": False}
+
+    response = client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_admin_can_add_shopping_items(create_user, create_shopping_list, admin_client):
+    user = create_user()
+    shopping_list = create_shopping_list(user)
+
+    url = reverse("add-shopping-item", kwargs={"pk": shopping_list.id})
+
+    data = {"name": "Milk", "purchased": False}
+
+    response = admin_client.post(url, data, format="json")
+
+    assert response.status_code == status.HTTP_201_CREATED
+
+
+@pytest.mark.django_db
+def test_shopping_item_is_retrieved_by_id(
+    create_user, create_authenticated_client, create_shopping_item
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=user)
 
     url = reverse(
         "shopping-item-detail",
         kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
     )
-    client = APIClient()
 
     response = client.get(url)
 
@@ -195,14 +344,55 @@ def test_shopping_item_is_retrieved_by_id(create_shopping_item):
 
 
 @pytest.mark.django_db
-def test_change_shopping_item_purchased_status(create_shopping_item):
-    shopping_item = create_shopping_item(name="Chocolate")
+def test_shopping_item_detail_access_restricted_if_not_member_of_shopping_list(
+    create_user, create_authenticated_client, create_shopping_item
+):
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=shopping_list_creator)
 
     url = reverse(
         "shopping-item-detail",
         kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
     )
-    client = APIClient()
+
+    response = client.get(url, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_admin_can_retrieve_single_shopping_item(
+    create_user, create_shopping_item, admin_client
+):
+    user = create_user()
+    shopping_item = create_shopping_item("Milk", user)
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+
+    response = admin_client.get(url)
+
+    assert response.status_code == status.HTTP_200_OK
+
+
+@pytest.mark.django_db
+def test_change_shopping_item_purchased_status(
+    create_user, create_authenticated_client, create_shopping_item
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=user)
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
 
     data = {"name": "Chocolate", "purchased": True}
     response = client.put(url, data, format="json")
@@ -213,15 +403,18 @@ def test_change_shopping_item_purchased_status(create_shopping_item):
 
 @pytest.mark.django_db
 def test_change_shopping_item_purchased_status_with_missing_data_returns_bad_request(
+    create_user,
+    create_authenticated_client,
     create_shopping_item,
 ):
-    shopping_item = create_shopping_item(name="Chocolate")
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=user)
 
     url = reverse(
         "shopping-item-detail",
         kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
     )
-    client = APIClient()
 
     data = {"purchased": True}
     response = client.put(url, data, format="json")
@@ -230,16 +423,42 @@ def test_change_shopping_item_purchased_status_with_missing_data_returns_bad_req
 
 
 @pytest.mark.django_db
-def test_change_shopping_item_purchased_status_with_partial_update(
-    create_shopping_item,
+def test_shopping_item_update_restricted_if_not_member_of_shopping_list(
+    create_user, create_authenticated_client, create_shopping_item
 ):
-    shopping_item = create_shopping_item(name="Chocolate")
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=shopping_list_creator)
 
     url = reverse(
         "shopping-item-detail",
         kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
     )
-    client = APIClient()
+
+    data = {"name": "Chocolate", "purchased": True}
+
+    response = client.put(url, data=data, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_change_shopping_item_purchased_status_with_partial_update(
+    create_user,
+    create_authenticated_client,
+    create_shopping_item,
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=user)
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
 
     data = {"purchased": True}
     response = client.patch(url, data, format="json")
@@ -249,16 +468,63 @@ def test_change_shopping_item_purchased_status_with_partial_update(
 
 
 @pytest.mark.django_db
-def test_shopping_item_is_deleted(create_shopping_item):
-    shopping_item = create_shopping_item(name="Chocolate")
+def test_shopping_item_partial_update_restricted_if_not_member_of_shopping_list(
+    create_user, create_authenticated_client, create_shopping_item
+):
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=shopping_list_creator)
 
     url = reverse(
         "shopping-item-detail",
         kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
     )
 
-    client = APIClient()
+    data = {"purchased": True}
+
+    response = client.patch(url, data=data, format="json")
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
+
+
+@pytest.mark.django_db
+def test_shopping_item_is_deleted(
+    create_user, create_authenticated_client, create_shopping_item
+):
+    user = create_user()
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=user)
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+
     response = client.delete(url)
 
     assert response.status_code == status.HTTP_204_NO_CONTENT
     assert len(ShoppingItem.objects.all()) == 0
+
+
+@pytest.mark.django_db
+def test_shopping_item_delete_restricted_if_not_member_of_shopping_list(
+    create_user, create_authenticated_client, create_shopping_item
+):
+    user = create_user()
+    shopping_list_creator = User.objects.create_user(
+        "Creator", "creator@list.com", "something"
+    )
+    client = create_authenticated_client(user)
+    shopping_item = create_shopping_item(name="Chocolate", user=shopping_list_creator)
+
+    url = reverse(
+        "shopping-item-detail",
+        kwargs={"pk": shopping_item.shopping_list.id, "item_pk": shopping_item.id},
+    )
+
+    response = client.delete(url)
+
+    assert response.status_code == status.HTTP_403_FORBIDDEN
