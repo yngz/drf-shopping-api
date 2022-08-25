b60a5f8e818beada48ff683e975a58f77122d70d
Yang Zou <yang0201@outlook.com>
2022-08-25 11:21:10 +0100

    Add custom permissions for views

--- shopping_list/api/views.py
+++ shopping_list/api/views.py
@@ -1,4 +1,9 @@
 from rest_framework import generics
+from shopping_list.api.permissions import (
+    AllShoppingItemsShoppingListMembersOnly,
+    ShoppingItemShoppingListMembersOnly,
+    ShoppingListMembersOnly,
+)
 from shopping_list.api.serializers import ShoppingItemSerializer, ShoppingListSerializer
 from shopping_list.models import ShoppingItem, ShoppingList
 
@@ -14,14 +19,17 @@ class ListAddShoppingList(generics.ListCreateAPIView):
 class ShoppingListDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShoppingList.objects.all()
     serializer_class = ShoppingListSerializer
+    permission_classes = [ShoppingListMembersOnly]
 
 
 class AddShoppingItem(generics.CreateAPIView):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
+    permission_classes = [AllShoppingItemsShoppingListMembersOnly]
 
 
 class ShoppingItemDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = ShoppingItem.objects.all()
     serializer_class = ShoppingItemSerializer
+    permission_classes = [ShoppingItemShoppingListMembersOnly]
     lookup_url_kwarg = "item_pk"
