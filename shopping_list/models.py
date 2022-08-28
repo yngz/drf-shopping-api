8a00ca5d58cb19cc2f0e3ae0d73a5665cfdc2f25
Yang Zou <yang0201@outlook.com>
2022-08-28 13:45:34 +0100

    Add last interaction field to shopping list for ordering

--- shopping_list/models.py
+++ shopping_list/models.py
@@ -7,6 +7,7 @@ class ShoppingList(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     members = models.ManyToManyField("auth.User")
+    last_interaction = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
