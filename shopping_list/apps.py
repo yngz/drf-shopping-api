6fe4691c729ffde87a1681914639a7eb7348e89e
Yang Zou <yang0201@outlook.com>
2022-08-28 13:46:44 +0100

    Create post_save signal from shopping item to shopping list

--- shopping_list/apps.py
+++ shopping_list/apps.py
@@ -2,5 +2,9 @@ from django.apps import AppConfig
 
 
 class ShoppingListConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'shopping_list'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "shopping_list"
+
+    def ready(self):
+        # implicitly connect signal handlers
+        import shopping_list.receivers
